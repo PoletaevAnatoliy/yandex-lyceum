81be1512fa79838f6e553a3f644e208a028c71ca
Anatoliy Poletaev <Shtepser@yandex.ru>
2020-11-17 16:52:31 +0300

    Added printing version

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
+def print_version():
+    print("Version")
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("This is release version 1.0")
