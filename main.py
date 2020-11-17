fa1232c5ec09639a7616845666a3af523657b5f0
Anatoliy Poletaev <anatoliy-poletaev@mail.ru>
2020-11-17 17:15:24 +0300

    I'v added new wonderful feature

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def print_version():
     print("Version")
 
 
+def wonderful_feature():
+    print("Sh up and take my money!")
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("This is release version 1.0")
